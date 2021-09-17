8809cfdad02638f65a8952e58c9cb7d22ba4fbea
Ashik Ghosh <ashikghosh763@gmail.com>
2021-09-17 14:54:19 +0600

    almost done

--- firebase.js
+++ firebase.js
@@ -1,11 +1,30 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCxRoZenF67CdOXiP7sy6Vlguxu12m8-48",
-    authDomain: "to-do-ashik.firebaseapp.com",
-    projectId: "to-do-ashik",
-    storageBucket: "to-do-ashik.appspot.com",
-    messagingSenderId: "1053197178910",
-    appId: "1:1053197178910:web:531b07366d2fed7ea1f60f",
-    measurementId: "G-194H3S0MC0"
-  };
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCxRoZenF67CdOXiP7sy6Vlguxu12m8-48",
+//     authDomain: "to-do-ashik.firebaseapp.com",
+//     projectId: "to-do-ashik",
+//     storageBucket: "to-do-ashik.appspot.com",
+//     messagingSenderId: "1053197178910",
+//     appId: "1:1053197178910:web:531b07366d2fed7ea1f60f",
+//     measurementId: "G-194H3S0MC0"
+//   };
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+//   var db = firebase.firestore();
+
+
+//to DO LIVE
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBw2JDYD4vVMonE7b1KbIAZCKXVWtAWbm4",
+  authDomain: "to-do-live-e8078.firebaseapp.com",
+  projectId: "to-do-live-e8078",
+  storageBucket: "to-do-live-e8078.appspot.com",
+  messagingSenderId: "269285965177",
+  appId: "1:269285965177:web:82f3d3fc9b0802cff843a8",
+  measurementId: "G-XB36BVXF15"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  var db = firebase.firestore();
--- script.js
+++ script.js
@@ -1,6 +1,92 @@
 function addItem(event) {
-    event.preventDefault();
-    // console.log("hello",event);
-    let text = document.getElementById('todo-input');
-  
+  event.preventDefault();
+  // console.log("hello",event);
+  let text = document.getElementById("todo-input");
+  db.collection("todo-items").add({
+    text: text.value,
+    status: "active",
+  });
+  text.value = "";
 }
+
+function getItems() {
+  db.collection("todo-items").onSnapshot((snapshot) => {
+    // console.log(snapshot.docs[0].data);
+    let items = [];
+    
+    snapshot.docs.forEach((doc) => {
+     
+      items.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+    console.log(items);
+    generateItems(items);
+  });
+}
+
+function generateItems(items) {
+  let itemsHTML = "";
+  items.forEach((item) => {
+    // console.log(item);
+    itemsHTML += `
+          <div class="todo-item">
+               <div class="check">
+                  <div data-id="${item.id}" class="check-mark ${item.status == "completed"? "checked": " "} ">
+                      <img src="./images/icon-check.svg" alt="">
+                  </div>
+               </div>
+               <div class="todo-text  ${item.status == "completed"? "checked": " "}">  
+          
+                 ${item.text}
+               </div>
+
+          </div>
+    
+    `;
+  });
+  document.querySelector(".todo-items").innerHTML = itemsHTML;
+  createEventListeners();
+}
+function createEventListeners() {
+  let todoCheckMarks = document.querySelectorAll(".todo-item .check ");
+  // console.log(todoCheckMarks);
+  todoCheckMarks.forEach((checkMark) => {  
+    console.log(checkMark);
+        checkMark.addEventListener("click", () => {
+          // console.log(checkMark.lastElementChild.attributes[0]);
+          let check = checkMark.querySelector(".check-mark").getAttribute("data-id");
+          // console.log(check);
+          markCompleted(check);
+          // console.dir(checkMark);
+         
+        })
+   })
+    // console.log(todoCheckMarks);
+
+}
+
+function markCompleted(id) {
+  console.log(id);
+  let item = db.collection("todo-items").doc(id);
+  item.get().then(function(doc) {
+    if(doc.exists){
+      // console.log("here is the doc",doc.data());
+      let status = doc.data().status;
+      if(status == "active") {
+        item.update({
+          status:"completed"
+        })
+      }
+      else if(status == "completed") {
+        item.update({
+          status:"active"
+        })
+
+
+      }
+    }
+  })
+}
+getItems();
