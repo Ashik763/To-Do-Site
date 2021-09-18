34593a30383c59e3b69e625e2697c5c4aadc0cfb
Ashik Ghosh <ashikghosh763@gmail.com>
2021-09-18 16:52:32 +0600

    project done

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function allItems(event) {
         ...doc.data(),
       });
     });
-    console.log(items);
+    // console.log(items);
     generateItems(items);
     numberOfItems(items.length);
     
@@ -79,7 +79,7 @@ function clearCompletedItems(event){
     snapshot.docs.forEach((doc) => {
       if(doc.data().status == "completed") {
        db.collection("todo-items").doc(doc.id).delete().then(() => {
-         console.log("successfull");
+        //  console.log("successfull");
        }).catch((error) => {
         console.error("Error removing document: ", error);
     });
@@ -140,7 +140,7 @@ function getItems() {
         ...doc.data(),
       });
     });
-    console.log(items);
+    // console.log(items);
     generateItems(items);
     numberOfItems(items.length);
     
@@ -154,7 +154,7 @@ function generateItems(items) {
   items.forEach((item) => {
     // console.log(item);
     itemsHTML += `
-          <div class="todo-item">
+          <div class="todo-item white">
                <div class="check">
                   <div data-id="${item.id}" class="check-mark ${item.status == "completed"? "checked": " "} ">
                       <img src="./images/icon-check.svg" alt="">
@@ -176,7 +176,7 @@ function createEventListeners() {
   let todoCheckMarks = document.querySelectorAll(".todo-item .check ");
   // console.log(todoCheckMarks);
   todoCheckMarks.forEach((checkMark) => {  
-    console.log(checkMark);
+    // console.log(checkMark);
         checkMark.addEventListener("click", () => {
           // console.log(checkMark.lastElementChild.attributes[0]);
           let check = checkMark.querySelector(".check-mark").getAttribute("data-id");
@@ -191,7 +191,7 @@ function createEventListeners() {
 }
 
 function markCompleted(id) {
-  console.log(id);
+  // console.log(id);
   let item = db.collection("todo-items").doc(id);
   item.get().then(function(doc) {
     if(doc.exists){
@@ -225,4 +225,85 @@ function numberOfItems(length) {
 
 }
 
+// function setTheme(){
+//   let background = document.getElementsByClassName("background-image")[0];
+//   let oImg = document.createElement("img");
+//   oImg.setAttribute('src', './images/bg-desktop-dark.jpg');
+//   oImg.setAttribute('id', 'image');
+//   oImg.setAttribute('alt', 'na');
+//   oImg.setAttribute('theme', 'dark');
+//   background.appendChild(oImg);
+
+// }
+// setTheme();
+
+function themeChange(event){
+  console.log("clicked");
+  
+  let background = document.getElementById("background-image");
+  let oImg1 = document.getElementById("image");
+  let theme = oImg1.getAttribute("theme");
+  console.log(theme);
+
+  // var element = document.getElementById(");
+  // oImg1.classList.toggle("mystyle");
+  
+  // oImg.setAttribute('theme', 'dark');
+  if(theme =="dark") {
+    console.log("dark er moddhe");
+    background.innerHTML = "";
+    let oImg = document.createElement("img");
+    oImg.setAttribute('src', './images/bg-desktop-light.jpg');
+    oImg.setAttribute('id', 'image');
+    oImg.setAttribute('alt', 'na');
+    oImg.setAttribute('theme', 'light');
+    let body = document.getElementsByClassName("body")[0];
+    console.log(body);
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+    let whites = document.getElementsByClassName("white");
+    for(i=0;i<whites.length;i++){
+      whites[i].style.backgroundColor = "white";
+      whites[i].style.color = "rgb(70, 78, 84)";
+    }
+    background.appendChild(oImg);
+    let sunMoon = document.getElementById("sun-moon");
+    sunMoon.innerHTML = "";
+    let sunMoonImg = document.createElement("img");
+    sunMoonImg.setAttribute('src','./images/icon-moon.svg');
+    sunMoonImg.setAttribute('alt','sun-icon');
+    sunMoon.appendChild(sunMoonImg);
+
+  }
+  else if(theme =="light") {
+    console.log("lighter moddhe");
+    background.innerHTML = "";
+    let oImg = document.createElement("img");
+    oImg.setAttribute('src', './images/bg-desktop-dark.jpg');
+    oImg.setAttribute('id', 'image');
+    oImg.setAttribute('alt', 'na');
+    oImg.setAttribute('theme', 'dark');
+    let body = document.getElementsByClassName("body")[0];
+    body.style.backgroundColor = "#181824";
+    body.style.color = "black";
+    let whites = document.getElementsByClassName("white");
+    for(i=0;i<whites.length;i++){
+      whites[i].style.backgroundColor = "#181824";
+      whites[i].style.color = "white";
+    }
+    
+    console.log(body);
+      background.appendChild(oImg) ;
+      let sunMoon = document.getElementById("sun-moon");
+    sunMoon.innerHTML = "";
+    let sunMoonImg = document.createElement("img");
+    sunMoonImg.setAttribute('src','./images/icon-sun.svg');
+    sunMoonImg.setAttribute('alt','sun-icon');
+    sunMoon.appendChild(sunMoonImg);
+  }
+ 
+  // oImg.setAttribute('theme', 'dark');
+  
+
+}
 
